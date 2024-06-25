cb32d6cac4cbfb37f870192631542ac5b0f40a9e
Alethepata <alessia.patane@gmail.com>
2024-06-25 18:52:58 +0200

    edit login

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,22 +1,47 @@
-import { createContext, useContext, useState } from "react";
+import axios from "../utils/axiosClient";
+import useStorage from "../hooks/useStorage";
+import { createContext, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-
 
 const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
-
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+    
     const navigate = useNavigate();
+    
 
-    const login = () => {
-        setIsLoggedIn(true);
-        navigate('/posts')
+    const [user, setUser] = useStorage(null, 'user');
+
+    const isLoggedIn = user !== null;
+
+
+    const login = async (payload) => {
+        try {
+            
+            const data = await axios.post('/auth/login', payload);
+            
+            setUser(data.data);
+            
+            localStorage.setItem('accessToken', response.token);
+            
+            navigate('/');
+
+        } catch (err) {
+
+            const errors = err.response.data.errors;
+            
+            const error = new Error(errors ? 'Errore login' : err.response.data);
+            
+            error.errors = errors;
+
+            throw errors;
+        }
     }
+
     const logout = () => {
-        setIsLoggedIn(false);
-        navigate('/login')
+        setUser(null);
+        localStorage.removeItem('accessToken');
+        navigate('/login');
     }
 
     const values = {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,70 @@
+import { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 
 function Login() {
 
     const { login } = useAuth();
 
-    const handleSubmit = (event) => { 
-        event.preventDefault();
-        login();
+    const initialData = {
+        password:"",
+        email: ""
+    };
+
+    const [formData, setFormData] = useState(initialData);
+
+    const [loginError, setLoginError] = useState(null);
+
+    const changeData = (key, value) => {
+        setFormData(curr => ({
+            ...curr,
+            [key]: value
+        }));
     }
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        try{
+            await login(formData);
+            setFormData(initialData);
+        } catch (err) {
+            setLoginError(err);
+        }
+    }
+
     return (
         <div className="container">
             <h1>Log In</h1>
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Nome</label>
-                    <input type="text" id="name"/>
+                    <input
+                        id="name"
+                        type="text"
+                        value={formData.email}
+                        onChange={e => changeData('email', e.target.value)}
+                    />
                 </div>
-
+    
                 <div className="margin">
                     <label htmlFor="lastname">Cognome</label>
-                    <input type="text" id="lastname"/>
+                    <input
+                        id="lastname"
+                        type="password"
+                        placeholder="Password" 
+                        value={formData.password}
+                        onChange={e => changeData('password', e.target.value)}
+                    />
                 </div>
 
+                {loginError !== null && <div className="error">{loginError.message}</div>}
+                {loginError?.errors && loginError.errors.map( (err, index) => (
+                <div key={`err${index}`}>{err.msg}</div>
+                ))}
+    
                 <button>Log in</button>
-
+    
             </form>
-        </div>
+       </div>
     )
 }
 
